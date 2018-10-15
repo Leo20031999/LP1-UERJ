5f12c09ea7876a62e48b8cd7b325efba5f6fd6e2
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2018-10-15 18:28:15 -0300

    Add files via upload

---
+++ Exercicios 5/ex5-3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+int compra(int* conta, int valor){
+	*conta=*conta-valor;
+}
+int main(void){
+	int compras[]={100,50,80,30,20};
+	int conta1,conta2;
+	int valor,i;
+	int* conta;
+	printf("Digite o saldo da conta 1:");
+	scanf("%i", &conta1);
+	printf("Digite o saldo da conta 2:");
+	scanf("%i", &conta2);
+	int minha_conta=conta1+conta2;
+	printf("Saldo total: %d\n",minha_conta);
+	for (i=0;i<5;i+=1){
+		valor=compras[i];
+		if(conta1>conta2){
+			conta=&conta1;
+		}else{
+			conta=&conta2;
+		}
+		compra(conta,valor);
+	}
+	int contatual=conta1+conta2;
+	printf("Conta 1: %d\nConta 2:%d \n",conta1,conta2);
+	printf("Saldo atual: %d\n", contatual);
+	return 0;
+}
