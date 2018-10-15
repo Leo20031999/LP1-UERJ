d21bc35320a4e80bad79c9bc0425d89709a137dd
Leo20031999 <43154655+Leo20031999@users.noreply.github.com>
2018-10-15 18:23:48 -0300

    Add files via upload

---
+++ Exercicios 5/5-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int compra(int* conta, int valor){
+	*conta=*conta-valor;
+}
+int main(void){
+	int conta1, conta2;
+	int minha_conta=0;
+	int contatual;
+	int *conta;
+	int valor=500;
+	printf("digite o saldo 1: ");
+	scanf("%d", &conta1);
+	printf("digite o saldo 2: ");
+	scanf("%d", &conta2);
+	minha_conta=conta1+conta2;
+	printf("Conta 1: %d \nConta 2: %d\n", conta1, conta2);
+	printf("Saldo total: %d\n", minha_conta);
+	if(conta1>conta2){
+		conta=&conta1;
+	}
+	else{
+		conta=&conta2;
+	}
+	compra(conta,valor);
+	contatual=conta1+conta2;
+	printf("Conta 1: %d \nConta 2: %d\n", conta1, conta2);
+	printf("Saldo atual: %d", contatual);
+	return 0;
+}
